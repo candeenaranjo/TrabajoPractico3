9c55ca654acb9ebd258987a996556049f0955718
Candela Naranjo <candenaranjo05@gmail.com>
2025-08-26 19:26:27 -0300

    Initial commit

---
+++ src/trabajopractico3/TrabajoPractico3.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package trabajopractico3;
+
+/**
+ *
+ * @author Candela Naranjo
+ */
+public class TrabajoPractico3 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Holaaa");
+        // TODO code application logic here
+    }
+    
+}
